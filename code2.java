8443f5d030bc7481093a6194164ddbd32adc24a5
Naga Venkata Durga Praneeth Gamidi <143397963+Praneeth-gamidi@users.noreply.github.com>
2026-03-09 09:40:13 +0530

    Home Work

--- code2.java
+++ code2.java
@@ -1,13 +1,42 @@
 import java.util.Scanner;
-public class Main {
+
+class Student {
+    int id;
+    String name;
+    int age;
+    String course;
+
+    void getDetails() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student ID: ");
+        id = sc.nextInt();
+        sc.nextLine(); 
+
+        System.out.print("Enter Student Name: ");
+        name = sc.nextLine();
+
+        System.out.print("Enter Age: ");
+        age = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Course: ");
+        course = sc.nextLine();
+    }
+
+    void displayDetails() {
+        System.out.println("\nStudent Details:");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+    }
+}
+
+public class StudentDetails {
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        int i=1;
-        do{
-            System.out.print(i);
-            i++;
-        }while(i<=6);
-         System.out.println();
-        System.out.println("Program Exited!");
+        Student s = new Student();
+        s.getDetails();
+        s.displayDetails();
     }
 }
